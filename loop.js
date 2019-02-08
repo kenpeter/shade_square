0517dc47ebd0827b6704eb52cbd6e332acde5736
kenpeter <kenpeter@kenpeter.com>
2019-02-08 23:55:29 +1100

    up

--- loop.js
+++ loop.js
@@ -21,8 +21,8 @@ function loopCount() {
 }
 
 function arrSum(arr) {
-  var sum = 0;
-  for (var i = 0; i < arr.length; i++) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
     if (typeof arr[i] == 'object')
       sum += arrSum(arr[i]);
     else
